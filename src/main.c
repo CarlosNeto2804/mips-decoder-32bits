76832d94e196439f804b66208a7c2c377e15fae6
Carlos Alberto Neto <carloshtcursos@gmail.com>
2020-04-13 22:05:35 -0400

    Remvendo Codigos desnecessarios

--- src/main.c
+++ src/main.c
@@ -5,31 +5,28 @@
 extern void encode(FILE *output, FILE *input);
 static void usage(void)
 {
-	fprintf(stderr, "mips-decode [input] [output]\n");
+	fprintf(stderr, "mips-pocket-decoder [input] [output]\n");
 	exit(EXIT_FAILURE);
 }
-
-int main(int argc, const char *argv[])
+int main(int argsc, const char *argsv[])
 {
 	const char *errorstr;
-	FILE *input, *output;
-	input = stdin;
-	output = stdout;
-
-	if (argc > 3)
+	FILE *input = stdin;
+	FILE *output = stdout;
+	if (argsc > 3)
 		usage();
 
-	if (argc == 3)
+	if (argsc == 3)
 	{
-		if ((input = fopen(argv[1], "r")) == NULL)
+		if ((input = fopen(argsv[1], "r")) == NULL)
 		{
-			errorstr = "cannot open input file";
+			errorstr = "Não foi possível abrir o arquivo de entrada";
 			goto error0;
 		}
 
-		if ((output = fopen(argv[2], "w")) == NULL)
+		if ((output = fopen(argsv[2], "w")) == NULL)
 		{
-			errorstr = "cannot create output file";
+			errorstr = "Não foi possivel criar o arquivo de saída";
 			goto error1;
 		}
 	}
